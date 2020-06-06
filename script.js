cab7584b1877c5eb05fc47ab9499c22f36f7a185
egrbOObs <egor01.6@inbox.ru>
2020-06-06 18:42:14 +0700

    Урок №1: задание

---
+++ script.js
@@ -0,0 +1,12 @@
+let money = 5000;
+let income = 4000;
+let addExpenses = 1000;
+let deposit = true;
+let mission = "Make the code";
+let period;
+
+alert("Какое-то сообщение!");
+
+console.log("Какое-то сообщение в консоли!");
+
+
