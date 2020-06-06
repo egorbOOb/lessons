3d8c2df7d0d687803db5a8816ad1803b6e189536
egrbOObs <egor01.6@inbox.ru>
2020-06-07 02:14:33 +0700

    Урок №2

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 150000;
+let income = 40000;
+let addExpenses = 'Интернет, оплата фриланс площадки, обслуживание машины, подписка на софт';
+let deposit = true;
+let mission = 12000000;
+let period = 12;
 
-alert("Какое-то сообщение!");
+console.log(typeof(money));
 
-console.log("Какое-то сообщение в консоли!");
+console.log(typeof(income));
 
+console.log(typeof(deposit));
 
+console.log(addExpenses.length);
+
+console.log('Период равен:', period, 'месяцев');
+
+console.log('Цель заработать:', mission, 'рублей');
+
+addExpenses.toLowerCase();
+
+console.log(addExpenses.split(', '));
+
+let budgetDay = 150000 + 40000 / 30;
+
+console.log(budgetDay);
