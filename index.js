985988765adf30145c1de78df57024e4574843b3
Egor Eliseev <egor01.6@inbox>
2020-06-18 02:39:27 +0700

    Урок №9

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let buttonStart = document.getElementById('start');
+
+let buttonPlusFirst = document.getElementsByTagName('button')[0];
+
+let buttonPlusSecond = document.getElementsByTagName('button')[1];
+
+let checkBox = document.querySelector('#deposit-check');
+
+let incomeItem = document.querySelectorAll('.additional_income-item');
+
+let budgetDayValue = document.getElementsByClassName('budget_day-value');
+
+let expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+
+let additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+
+let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+
+let incomePeriodValue = document.getElementsByClassName('income_period-value');
+
+let targetMonthValue = document.getElementsByClassName('target_month-value');
+
+let incomeTitle = document.querySelector('.income-title');
+
+let additionalIncomeItem = document.querySelector('.additional_income-item');
+
+let rangeTitle = document.querySelector('.period-select');
+
+console.log(incomeItem);
+
+console.log(buttonStart);
+
+console.log(buttonPlusFirst);
+
+console.log(buttonPlusSecond);
+
+console.log(checkBox);
+
+console.log(incomeItem);
+
+console.log(budgetDayValue);
+
+console.log(expensesMonthValue);
+
+console.log(additionalIncomeValue);
+
+console.log(additionalExpensesValue);
+
+console.log(incomePeriodValue);
+
+console.log(targetMonthValue);
+
+console.log(incomeTitle);
+
+console.log(additionalIncomeItem);
+
+console.log(rangeTitle);
